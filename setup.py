86baedcc674a9b0f28b3784681a7eea9032085b8
Russell Keith-Magee <russell@keith-magee.com>
2014-08-16 13:01:21 +0800

    Updates to packaging and versioning strategy.

--- setup.py
+++ setup.py
@@ -1,27 +1,31 @@
 #/usr/bin/env python
-from setuptools import setup
-from cassowary import VERSION
+import io
+import re
+from setuptools import setup, find_packages
 
-try:
-    readme = open('README.rst')
-    long_description = str(readme.read())
-finally:
-    readme.close()
 
-required_pkgs = []
+with io.open('./cassowary/__init__.py', encoding='utf8') as version_file:
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file.read(), re.M)
+    if version_match:
+        version = version_match.group(1)
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+with io.open('README.rst', encoding='utf8') as readme:
+    long_description = readme.read()
+
 
 setup(
     name='cassowary',
-    version=VERSION,
+    version=version,
     description='A pure Python implementation of the Cassowary constraint solving algorithm.',
     long_description=long_description,
     author='Russell Keith-Magee',
     author_email='russell@keith-magee.com',
     url='http://pybee.org/cassowary',
-    packages=[
-        'cassowary',
-    ],
-    install_requires=required_pkgs,
+    packages=find_packages(exclude=['docs', 'tests']),
+    install_requires=[],
     license='New BSD',
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -33,6 +37,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Scientific/Engineering :: Mathematics',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
